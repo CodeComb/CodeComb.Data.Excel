aeb72d518555acd73eae5165e0e39a2808d15d33
あまみや ゆうこ <1@1234.sh>
2016-01-07 15:24:19 +0800

    Implementing infrastructures

---
+++ src/CodeComb.Data.Excel/Infrastructure/SharedStrings.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace CodeComb.Data.Excel.Infrastructure
+{
+    public class SharedStrings : IList<string>, IDisposable
+    {
+        private string xmlSource;
+
+        private Dictionary<ulong, string> dic = new Dictionary<ulong, string>();
+
+        public int Count
+        {
+            get
+            {
+                return dic.Count;
+            }
+        }
+
+        public bool IsReadOnly
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public string this[int index]
+        {
+            get
+            {
+                return this[Convert.ToUInt64(index)];
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public SharedStrings(string XmlSource)
+        {
+            xmlSource = XmlSource;
+            var xd = new XmlDocument();
+            xd.LoadXml(xmlSource);
+            var t = xd.GetElementsByTagName("t");
+            ulong i = 0;
+            foreach (XmlNode x in t)
+                dic.Add(i++, x.Value);
+            xmlSource = null;
+            GC.Collect();
+        }
+
+        public string this[ulong index]
+        {
+            get
+            {
+                return dic[index];
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Dispose()
+        {
+            dic.Clear();
+            GC.Collect();
+        }
+
+        public int IndexOf(string item)
+        {
+            var index = dic.Where(x => x.Value == item)
+                .Select(x => x.Key)
+                .FirstOrDefault();
+            return Convert.ToInt32(index);
+        }
+
+        public void Insert(int index, string item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(string item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(string item)
+        {
+            return dic.Any(x => x.Value == item);
+        }
+
+        public void CopyTo(string[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(string item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            return dic.Select(x => x.Value)
+                .ToList()
+                .GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ src/CodeComb.Data.Excel/Infrastructure/Sheet.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Common;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace CodeComb.Data.Excel.Infrastructure
+{
+    public class Sheet
+    {
+        private string xmlSource;
+
+        private List<List<string>> table = new List<List<string>>();
+
+        public Sheet(string XmlSource, SharedStrings StringDictionary)
+        {
+            var xd = new XmlDocument();
+            xd.LoadXml(XmlSource);
+            var rows = xd.GetElementsByTagName("row");
+            // 遍历row标签
+            foreach(XmlNode x in rows)
+            {
+                var cols = x.ChildNodes;
+                var objs = new List<string>();
+                // 遍历c标签
+                foreach (XmlNode y in cols)
+                {
+                    string value = null;
+                    // 如果是字符串类型，则需要从字典中查询
+                    foreach (XmlAttribute z in y.Attributes)
+                    {
+                        if (z.Name == "t" && z.Value == "s")
+                        {
+                            var index = Convert.ToUInt64(y.FirstChild.Value);
+                            value = StringDictionary[index];
+                            break;
+                        }
+                    }
+                    // 否则其中的v标签值即为单元格内容
+                    if (string.IsNullOrEmpty(value))
+                        value = y.FirstChild.Value;
+                    
+                    objs.Add(value);
+                }
+
+                // 去掉末尾的null
+                while (objs.LastOrDefault() == null)
+                    objs.RemoveAt(objs.Count - 1);
+                table.Add(objs);
+                GC.Collect();
+            }
+        }
+    }
+}
