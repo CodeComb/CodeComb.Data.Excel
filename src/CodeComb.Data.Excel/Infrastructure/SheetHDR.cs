95c1a5d3952174e763f887d11a481203ce4826b3
あまみや ゆうこ <1@1234.sh>
2016-03-13 21:36:30 +0800

    Fix HDR

--- src/CodeComb.Data.Excel/Infrastructure/SheetHDR.cs
+++ src/CodeComb.Data.Excel/Infrastructure/SheetHDR.cs
@@ -63,6 +63,8 @@ namespace CodeComb.Data.Excel.Infrastructure
                 // 去掉末尾的null
                 while (objs.LastOrDefault() == null)
                     objs.RemoveAt(objs.Count - 1);
+                if (objs.Count > 0)
+                    this.Add(objs);
             }
             while (this.Count > 0 && this.Last().Count == 0)
                 this.RemoveAt(this.Count - 1);
